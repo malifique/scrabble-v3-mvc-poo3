f0a0e8f6030a98aef2e50b6e444a4899b0db0024
mathroule <mathroule@f3b3de2c-2763-11df-a6dc-d9c9baf28c7f>
2010-03-22 14:12:01 +0000

    Modif de l'affichage du rack

--- src/game/scrabble/view/RackPanel.java
+++ src/game/scrabble/view/RackPanel.java
@@ -3,8 +3,13 @@ package game.scrabble.view;
 import game.scrabble.controller.Controller;
 import game.scrabble.controller.RackChangedEvent;
 import game.scrabble.controller.RackListener;
+import game.scrabble.model.Board;
+import game.scrabble.model.Letter;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.LayoutManager;
 
 import javax.swing.JLabel;
@@ -12,28 +17,34 @@ import javax.swing.JPanel;
 
 public class RackPanel extends JPanel implements RackListener {
 
-    /**
+	/**
      * 
      */
-    private static final long serialVersionUID = 1L;
-    private JLabel rack; 
-    
-    public RackPanel() {
-        LayoutManager layout = new FlowLayout();
-        this.setLayout(layout);
-        // créer un label rack par joueur, l'ajouter en listener.
-        rack = new JLabel("Rack "+"");
-        add(rack);
-    }
-    
-    public RackPanel(Controller c) {
-        this();
-        c.addRackListener(this);
-    }
+	private static final long serialVersionUID = 1L;
+	private JPanel rack;
 
-    @Override
-    public void rackChanged(RackChangedEvent event) {
-        rack.setText("Rack "+event.getLetters());
-    }
+	public RackPanel() {
+		setLayout(new FlowLayout());
+		rack = new JPanel();
+		rack.setLayout(new GridLayout(1, 7, 5, 0));
+		add(new JLabel("Rack "));
+		add(rack);
+	}
+
+	public RackPanel(Controller c) {
+		this();
+		c.addRackListener(this);
+	}
+
+	@Override
+	public void rackChanged(RackChangedEvent event) {
+		rack.removeAll();
+
+		for (Letter letter : event.getLetters()) {
+			CaseButton cb = new CaseButton(letter.toString(), 0, 0);
+			cb.setBackground(new Color(255, 203, 96));
+			rack.add(cb);
+		}
+	}
 
 }
